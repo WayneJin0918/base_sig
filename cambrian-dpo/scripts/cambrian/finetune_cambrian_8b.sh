698b84727f08220061e931a0802cb8a7ad9e8a83
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-07-17 07:39:38 +0800

    Update finetune_cambrian_8b.sh

--- cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
+++ cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
@@ -26,7 +26,7 @@ python cambrian/train/train_tpu.py \
     --start_of_vision_sampler_layers 0 \
     --stride_of_vision_sampler_layers 3 \
     --mm_projector_type sva \
-    --unfreeze_mm_vision_tower False \
+    --unfreeze_mm_vision_tower True \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
@@ -40,7 +40,7 @@ python cambrian/train/train_tpu.py \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 3000 \
+    --save_steps 100 \
     --save_total_limit 1 \
     --learning_rate 4e-5 \
     --weight_decay 0. \
