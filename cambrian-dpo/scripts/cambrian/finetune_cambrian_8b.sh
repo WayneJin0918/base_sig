d7bd99a6b44d24ab77effee3df335a27ce3e56b1
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-07-17 23:38:17 +0800

    Update finetune_cambrian_8b.sh

--- cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
+++ cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
@@ -49,7 +49,7 @@ python cambrian/train/train_tpu.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --tf32 False \
-    --model_max_length 2048 \
+    --model_max_length 6144 \
     --gradient_checkpointing True \
     --dataloader_num_workers 8 \
     --lazy_preprocess True \
