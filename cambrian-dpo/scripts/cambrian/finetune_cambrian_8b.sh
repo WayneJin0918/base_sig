fe35b80959dcb8c9ec6c50d9d2b6d5592ba84934
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-07-17 10:47:42 +0800

    Update finetune_cambrian_8b.sh

--- cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
+++ cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
@@ -35,8 +35,8 @@ python cambrian/train/train_tpu.py \
     --bf16 True \
     --output_dir $CKPT_DIR \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 2 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
@@ -50,7 +50,7 @@ python cambrian/train/train_tpu.py \
     --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 8 \
     --lazy_preprocess True \
     --report_to wandb \
     --run_name $CKPT_NAME \
