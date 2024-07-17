8c0cd394355d8de8cfa0bb2ec0e1e45f097a0d26
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-07-17 14:48:16 +0800

    Update finetune_cambrian_8b.sh

--- cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
+++ cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
@@ -51,7 +51,7 @@ python cambrian/train/train_tpu.py \
     --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 8 \
+    --dataloader_num_workers 16 \
     --lazy_preprocess True \
     --report_to wandb \
     --run_name $CKPT_NAME \
