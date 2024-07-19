0ad2c60f45fbbf1dd46f88cca9f6553eb51e7ffa
WayneJin0918 <wayneyjin@gmail.com>
2024-07-19 05:41:15 +0000

    fix18

--- cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
+++ cambrian-dpo/scripts/cambrian/finetune_cambrian_8b.sh
@@ -11,7 +11,7 @@ export CKPT_DIR="/home/wayneyjin/model_ckpt/$CKPT_NAME" &&
 python cambrian/train/train_tpu.py \
     --model_name_or_path /home/wayneyjin/weiyangrl-bucket/cambrian/cambrian-8b \
     --version llama_v3 \
-    --data_path /home/wayneyjin/Cambrian7M.jsonl \
+    --data_path /home/wayneyjin/Cambrian7M_withsystemprompt.jsonl \
     --image_folder /home/wayneyjin/weiyangrl-bucket/data/finetune_data \
     --pretrain_mm_mlp_adapter /home/wayneyjin/model_ckpt/cambrian-8b-pretrain/mm_projector.bin \
     --vision_tower_aux_list '["siglip/CLIP-ViT-SO400M-14-384"]' \
