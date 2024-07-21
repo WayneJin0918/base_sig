822c46693c3472542950b0b2a704bd4e0bcd2371
WayneJin0918 <wayneyjin@gmail.com>
2024-07-21 01:38:10 +0000

    data

---
+++ base_sig/,erge_checkpoints.bash
@@ -0,0 +1,6 @@
+python /home/wayneyjin/merge_checkpoints.py \
+  --ckpt_prefix "/home/wayneyjin/ckpt/cambrian-8b-pretrain-all/checkpoint-3926/weights_rank" \
+  --output_dir "/home/wayneyjin/ckpt/merged_model" \
+  --config_name "/home/wayneyjin/ckpt/cambrian-8b-pretrain-all/config.json" \
+  --tokenizer_name "/home/wayneyjin/ckpt/cambrian-8b-pretrain-all/tokenizer" \
+  --num_shards 64
