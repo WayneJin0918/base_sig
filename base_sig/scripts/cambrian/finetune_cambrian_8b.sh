9a7537cd23742f8d7bb705d1a122027863946c81
WayneJin0918 <wayneyjin@gmail.com>
2024-07-27 17:44:39 +0000

    2

--- base_sig/scripts/cambrian/finetune_cambrian_8b.sh
+++ base_sig/scripts/cambrian/finetune_cambrian_8b.sh
@@ -3,7 +3,7 @@ export PJRT_DEVICE=TPU &&
 # export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export CKPT_NAME="cambrian-8b-finetune-llm-base" &&
-# export XLA_FLAGS="--xla_hlo_profile --xla_gpu_force_compilation_parallelism=1" &&
+export XLA_FLAGS="--xla_hlo_profile --xla_gpu_force_compilation_parallelism=1" &&
 
 export CKPT_DIR="/home/wayneyjin/ckpt/$CKPT_NAME" &&
 
