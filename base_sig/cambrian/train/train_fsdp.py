5cc6e77385b998bb1261020f72a2dd329ce5ae38
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-09-22 00:31:59 +0800

    Update train_fsdp.py

--- base_sig/cambrian/train/train_fsdp.py
+++ base_sig/cambrian/train/train_fsdp.py
@@ -1639,10 +1639,9 @@ def train(INDEX, attn_implementation=None):
     model.config.use_cache = False
     model.generation_config.do_sample = True
     
-    model.requires_grad_(False)
-    model.model.requires_grad_(True)
+    model.requires_grad_(True)
     if model_args.freeze_backbone:
-        model.model.requires_grad_(False)
+        model.requires_grad_(False)
 
     log_rank0("Model loaded.")
 
