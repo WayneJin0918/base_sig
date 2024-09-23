d0d09b24ad1b8e20671ccefaa7eeab9566b388fb
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-09-24 07:57:27 +0800

    Update train_fsdp.py

--- base_sig/cambrian/train/train_fsdp.py
+++ base_sig/cambrian/train/train_fsdp.py
@@ -1775,8 +1775,11 @@ def train(INDEX, attn_implementation=None):
                     param.requires_grad = True
 
         if training_args.tune_mm_projector:
-            for p in model.get_model().mm_projector.parameters():
-                p.requires_grad = True
+            tune_modules = ['mm_projector']
+            for name, param in model.named_parameters():
+                if any(listed_name in name for listed_name in tune_modules):
+                    print_rank0('tuning {}'.format(name))
+                    param.requires_grad = True
         
         model.config.freeze_mm_mlp_adapter = training_args.freeze_mm_mlp_adapter
         if training_args.freeze_mm_mlp_adapter:
