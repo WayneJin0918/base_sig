9fdc13c672de7887c2c33bb8177181c4b4510e2b
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-09-24 22:08:13 +0800

    Update finetuning_cambrian_8b_737k_dpo.sh

--- base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_737k_dpo.sh
+++ base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_737k_dpo.sh
@@ -76,8 +76,8 @@ TRAIN_ARGS="
     --bf16 True \
     --output_dir gs://weiyang2/$CKPT_NAME \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 2 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy no \
     --save_strategy steps \
