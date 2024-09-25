84218f881e5d64de56e8df1aa96e1667df6192d5
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-09-25 13:06:23 +0800

    Update finetuning_cambrian_8b_base_0p1_7m_posttrain_737k.bash

--- base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_base_0p1_7m_posttrain_737k.bash
+++ base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_base_0p1_7m_posttrain_737k.bash
@@ -30,7 +30,7 @@ export WANDB_NAME=$exp_name
 
 # Default values
 
-resume="gs://weiyang2/cambrian-8b-finetune-llm-dpo-posttrain-737k-dpo-2e-7-llm-on-vision-on-new-proj"
+resume=""
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
