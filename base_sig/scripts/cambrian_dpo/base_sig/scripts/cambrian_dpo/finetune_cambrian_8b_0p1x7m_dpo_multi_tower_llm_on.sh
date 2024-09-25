fbcacb4df9578e4d430caa49aad47def63df31c9
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-09-26 07:23:54 +0800

    Update finetune_cambrian_8b_0p1x7m_dpo_multi_tower_llm_on.sh

--- base_sig/scripts/cambrian_dpo/base_sig/scripts/cambrian_dpo/finetune_cambrian_8b_0p1x7m_dpo_multi_tower_llm_on.sh
+++ base_sig/scripts/cambrian_dpo/base_sig/scripts/cambrian_dpo/finetune_cambrian_8b_0p1x7m_dpo_multi_tower_llm_on.sh
@@ -113,7 +113,60 @@ fi
 echo $TRAIN_ARGS
 
 python cambrian/train/train_tpu.py \
-    $TRAIN_ARGS
+    --model_name_or_path /home/wayneyjin/weiyangrl-bucket/cambrian/cambrian-8b \
+    --version llama_v3 \
+    --data_path /home/wayneyjin/ckpt/Cambrian7M_withsystemprompt.jsonl \
+    --image_folder /home/wayneyjin/weiyangrl-bucket/data/finetune_data \
+    --pretrain_mm_mlp_adapter /home/wayneyjin/mm_projector.bin \
+    --vision_tower_aux_list '["siglip/CLIP-ViT-SO400M-14-384", "openai/clip-vit-large-patch14-336", "facebook/dinov2-giant-res378", "clip-convnext-XXL-multi-stage"]' \
+    --vision_tower_aux_token_len_list '[576, 576, 576, 9216]' \
+    --image_token_len 576 \
+    --num_query_group 1 \
+    --query_num_list [576] \
+    --connector_depth 3 \
+    --image_position 91 \
+    --vision_hidden_size 1024 \
+    --connector_only False \
+    --num_of_vision_sampler_layers 10 \
+    --start_of_vision_sampler_layers 0 \
+    --stride_of_vision_sampler_layers 3 \
+    --mm_projector_type sva \
+    --unfreeze_mm_vision_tower False \
+    --tune_mm_projector True \
+    --mm_projector_lr 4e-5 \
+    --freeze_backbone False \
+    --mm_vision_tower_lr 4e-5 \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir gs://weiyang2/$CKPT_NAME \
+    --num_train_epochs 0.1 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy no \
+    --save_strategy steps \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 4e-7 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type cosine \
+    --logging_steps 1 \
+    --tf32 False \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers ${num_workers:-4} \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --run_name $CKPT_NAME \
+    --fsdp full_shard \
+    --fsdp_config fsdp_config.json \
+    --dpo True \
+    --noise_level [0,50] \
 
 CKPT_PATH=checkpoints/$CKPT_NAME
 # check if the checkpoint path exists
