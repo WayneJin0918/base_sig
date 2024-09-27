8db923ba558911c2afbc237316a08ddfe387542e
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-09-27 22:52:09 +0800

    Update finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash

--- base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash
+++ base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash
@@ -76,7 +76,7 @@ TRAIN_ARGS="
     --group_by_modality_length True \
     --bf16 True \
     --output_dir gs://weiyang2/$CKPT_NAME \
-    --num_train_epochs 1 \
+    --num_train_epochs 0.1 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
     --gradient_accumulation_steps 1 \
