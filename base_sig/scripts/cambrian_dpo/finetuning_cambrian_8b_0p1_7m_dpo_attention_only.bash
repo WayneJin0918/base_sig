67a33e89e058dfd3b5f9d87d88021b19ade5420b
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-09-30 04:00:05 +0800

    Update finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash

--- base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash
+++ base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash
@@ -100,7 +100,7 @@ TRAIN_ARGS="
     --fsdp_config fsdp_config.json \
     --dpo True \
     --noise_level [0,75] \
-    --noise_type random_mask
+    --noise_type random_mask \
 "
 
 if [ -n "$resume" ]; then
