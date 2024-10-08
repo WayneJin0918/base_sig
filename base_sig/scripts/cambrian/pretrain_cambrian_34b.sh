a1a8faed10cf364bc549b3e70a38f3c6807998b7
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-10-08 21:35:28 +0800

    Update pretrain_cambrian_34b.sh

--- base_sig/scripts/cambrian/pretrain_cambrian_34b.sh
+++ base_sig/scripts/cambrian/pretrain_cambrian_34b.sh
@@ -5,15 +5,13 @@ export XLA_USE_BF16=0 &&
 export WANDB_RESUME="allow" &&
 export CKPT_NAME="cambrian-34b-pretrain" &&
 
-export CKPT_DIR="gs://us-central2-storage/cambrian/checkpoints/$CKPT_NAME" &&
-
 python cambrian/train/train_tpu.py \
-    --model_name_or_path NousResearch/Nous-Hermes-2-Yi-34B \
+    --model_name_or_path /home/wayneyjin/weiyangrl-bucket/llm_ckpts/Nous-Hermes-2-Yi-34B \
     --version chatml_direct \
-    --data_path your_path_to_pretrain_jsonl e.g. alignment_2.5m.jsonl \
-    --image_folder your_path_to_image_folder \
-    --vision_tower_aux_list '["siglip/CLIP-ViT-SO400M-14-384", "openai/clip-vit-large-patch14-336", "facebook/dinov2-giant-res378", "clip-convnext-XXL-multi-stage"]' \
-    --vision_tower_aux_token_len_list '[576, 576, 576, 9216]' \
+    --data_path /home/wayneyjin/alignment_2.5m.jsonl \
+    --image_folder /home/wayneyjin/weiyangrl-bucket/data/pretrain_data \
+    --vision_tower_aux_list '["siglip/CLIP-ViT-SO400M-14-384"]' \
+    --vision_tower_aux_token_len_list '[576]' \
     --image_token_len 576 \
     --num_query_group 1 \
     --query_num_list '[576]' \
@@ -32,7 +30,7 @@ python cambrian/train/train_tpu.py \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
     --bf16 False \
-    --output_dir $CKPT_DIR \
+    --output_dir gs://weiyang2/$CKPT_NAME \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
@@ -64,5 +62,5 @@ if [ ! -d "$CKPT_PATH" ]; then
     exit 1
 fi
 echo "Training finished. Syncing checkpoints to GCS..."
-gcloud alpha storage rsync $CKPT_PATH gs://us-central2-storage/cambrian/checkpoints/$CKPT_NAME
-echo "Syncing finished. Checkpoints are now available at gs://us-central2-storage/cambrian/checkpoints/$CKPT_NAME"
+gcloud alpha storage rsync $CKPT_PATH  gs://weiyang2/$CKPT_NAME/checkpoint-last
+echo "Syncing finished. Checkpoints are now available at gs://weiyang2/$CKPT_NAME/checkpoint-last"
