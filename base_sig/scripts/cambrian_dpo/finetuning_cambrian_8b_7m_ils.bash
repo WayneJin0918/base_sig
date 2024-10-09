0b675d55bbe04706c8e6d9be3e77148650f3b549
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-10-10 07:26:04 +0800

    Update finetuning_cambrian_8b_7m_ils.bash for better ft replace

--- base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_7m_ils.bash
+++ base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_7m_ils.bash
@@ -3,7 +3,7 @@ export PJRT_DEVICE=TPU
 export XLA_USE_BF16=1
 # export XLA_USE_BF16=0 &&
 # export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian-8b-finetune-llm-finetuning-0p1-7m-dpo-2e-7-l-on-v-on-new-proj-salt-25"
+export CKPT_NAME="cambrian-8b-finetune-llm-finetuning-0p1-7m-dpo-6e-7-l-on-v-on-new-proj-salt-25"
 # export XLA_FLAGS="--xla_hlo_profile --xla_gpu_force_compilation_parallelism=1"
 
 export CKPT_DIR="$HOME/ckpt/$CKPT_NAME"
@@ -30,7 +30,7 @@ export WANDB_NAME=$exp_name
 
 # Default values
 
-resume="/home/wayneyjin/cambrian-8b-finetune-llm-finetuning-0p1-7m-dpo-2e-7-l-on-v-on-new-proj-salt-25/checkpoint-10000"
+resume=""
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -84,7 +84,7 @@ TRAIN_ARGS="
     --save_strategy steps \
     --save_steps 1000 \
     --save_total_limit 1 \
-    --learning_rate 2e-7 \
+    --learning_rate 6e-7 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type cosine \
@@ -99,6 +99,7 @@ TRAIN_ARGS="
     --fsdp full_shard \
     --fsdp_config fsdp_config.json \
     --dpo True \
+    --beta 0.667 \
     --noise_level [0,25] \
     --noise_type salt_and_pepper \
 "
