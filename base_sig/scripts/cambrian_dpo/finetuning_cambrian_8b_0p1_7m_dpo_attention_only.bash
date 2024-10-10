013ec56734df7e15c602904a0d7dcaf064079da3
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-10-10 09:13:40 +0800

    Update finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash for mask 100%

--- base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash
+++ base_sig/scripts/cambrian_dpo/finetuning_cambrian_8b_0p1_7m_dpo_attention_only.bash
@@ -3,7 +3,7 @@ export PJRT_DEVICE=TPU
 export XLA_USE_BF16=1
 # export XLA_USE_BF16=0 &&
 # export WANDB_RESUME="allow" &&
-export CKPT_NAME="cambrian-8b-finetune-llm-dpo-posttrain-0p1-7m-dpo-7e-7-l-on-v-on-new-proj-salt-25-co-0.5-2"
+export CKPT_NAME="cambrian-8b-finetune-llm-dpo-posttrain-0p1-7m-dpo-7e-7-l-on-v-on-new-proj-mask-100"
 # export XLA_FLAGS="--xla_hlo_profile --xla_gpu_force_compilation_parallelism=1"
 
 export CKPT_DIR="$HOME/ckpt/$CKPT_NAME"
@@ -99,8 +99,8 @@ TRAIN_ARGS="
     --fsdp full_shard \
     --fsdp_config fsdp_config.json \
     --dpo True \
-    --beta 0.5 \
-    --noise_level [0,25] \
+    --beta 1 \
+    --noise_level [0,100] \
     --noise_type salt_and_pepper \
 "
 
