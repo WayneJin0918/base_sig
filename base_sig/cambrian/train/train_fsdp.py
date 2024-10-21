24b19c4cbfd73b235a68ed2da8e4a051848797ad
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-10-21 12:58:31 +0800

    Update train_fsdp.py

--- base_sig/cambrian/train/train_fsdp.py
+++ base_sig/cambrian/train/train_fsdp.py
@@ -1445,12 +1445,6 @@ class DataCollatorForSupervisedDataset(object):
                 cur_attention_mask_tmp[image_position+1:] = attention_mask[i, image_position:-1]
                 cur_attention_mask_tmp[image_position] = False
                 attention_mask[i] = cur_attention_mask_tmp
-
-        for i in range(len(input_ids)):
-            if i % 2 == 1:  # Odd index
-                image_token_indices = (input_ids[i] == IMAGE_TOKEN_INDEX).nonzero(as_tuple=True)[0]
-                if len(image_token_indices) > 0:
-                    attention_mask[i, image_token_indices] = False
         
         image_sizes = [instance['image_size'] for instance in instances]
         new_input_ids, new_labels, new_attention_mask, new_position_ids, im_aux_attention_masks_list = prepare_multimodal_data(input_ids, labels, attention_mask, image_sizes, image_token_len, image_aux_token_len_list, max_length)
