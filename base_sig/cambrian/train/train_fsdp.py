64751b26f8654b2995e85c109a474f79a7668999
Weiyang Jin <137654456+WayneJin0918@users.noreply.github.com>
2024-10-21 08:03:45 +0800

    Update train_fsdp.py

--- base_sig/cambrian/train/train_fsdp.py
+++ base_sig/cambrian/train/train_fsdp.py
@@ -14,7 +14,7 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
-from patch_metadata import patch_metadata
+from cambrian.train.patch_metadata import patch_metadata
 patch_metadata()
 import os
 # os.environ.pop('TPU_PROCESS_ADDRESSES') 
